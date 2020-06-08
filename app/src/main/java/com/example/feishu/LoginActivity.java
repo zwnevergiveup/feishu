be770644ee8ebf9e32960ca85330e14dd17063f3
zw <1052617797@qq.com>
2020-06-08 19:04:03 +0800

    init feishu

---
+++ app/src/main/java/com/example/feishu/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.feishu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginOrRegister(View view) {
+
+    }
+
+}
