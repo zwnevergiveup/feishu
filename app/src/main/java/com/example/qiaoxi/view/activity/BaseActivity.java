826d26347801a824f388152b2c53092e002ddbd2
zw <1052617797@qq.com>
2020-08-06 22:36:28 +0800

    沉浸式体验调整。

--- app/src/main/java/com/example/qiaoxi/view/activity/BaseActivity.java
+++ app/src/main/java/com/example/qiaoxi/view/activity/BaseActivity.java
@@ -25,15 +25,25 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        int option = window.getDecorView().getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-        window.getDecorView().setSystemUiVisibility(option);
-        window.setStatusBarColor(Color.TRANSPARENT);
         setupDataBinding();
         setupView();
         setupEvent();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        int option = window.getDecorView().getSystemUiVisibility()
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                ;
+        window.getDecorView().setSystemUiVisibility(option);
+        window.setStatusBarColor(Color.TRANSPARENT);
     }
 
     protected abstract void setupView();
